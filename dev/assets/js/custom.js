f1bfbdd255fad59bfc38fba67fefdfc988c2b79d
RostSuslov <suslovros@gmail.com>
2024-02-16 21:47:43 +0200

    feat: block information done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,31 @@
-  
+const installGenplan = () => {
+    const address = document.querySelector('#address')
+    const floor = document.querySelector('#floor')
+    const flat = document.querySelector('#flat')
+    const builds = document.querySelectorAll('.build-path')
+
+
+    builds.forEach(build => {
+        build.addEventListener('mouseover', () => {
+            const buildAddress = build.getAttribute('data-address')
+            const buildFloor = build.getAttribute('data-floor-quantity')
+            const buildFlat = build.getAttribute('data-flat-quantity')
+
+            address.innerHTML = buildAddress;
+            floor.innerHTML = buildFloor;
+            flat.innerHTML = buildFlat;
+        })
+    })
+}
+
+document.querySelector('.genplan') ? installGenplan() : null
+
+const headerNav = document.querySelector('.header-nav')
+const burger = document.querySelector('.btn-burger')
+
+console.log(headerNav);
+
+burger.addEventListener('click', () => {
+    console.log(burger);
+    headerNav.classList.toggle('show')
+})
